136a0b0e494016ab19205d07a8ffcc1ee96fc276
AalbatrossGuy <thexcelsiorisback@gmail.com>
2023-10-18 08:56:51 +0530

    /profile created and added param length check

--- server/NitrixAPI.py
+++ server/NitrixAPI.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Path
+from typing import Annotated
 
 nitrix = FastAPI()
 
@@ -6,3 +7,9 @@ nitrix = FastAPI()
 @nitrix.get('/')
 async def home():
     return {'message': 'Run Nitrix'}
+
+
+@nitrix.get('/profile/{pid}')
+async def _get_profile_data(pid: Annotated[str, Path(max_length=3)]):
+
+    return {"profile": pid}
